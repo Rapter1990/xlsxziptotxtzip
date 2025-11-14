0eb4c28ee870d65df10943957ac51ec00df15c11
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2025-11-14 22:03:11 +0300

    Task 1 : Initial commit

---
+++ src/main/java/com/example/xlsxziptotxtzip/XlsxZipToTxtZipApplication.java
@@ -0,0 +1,13 @@
+package com.example.xlsxziptotxtzip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XlsxZipToTxtZipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XlsxZipToTxtZipApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/xlsxziptotxtzip/XlsxZipToTxtZipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.xlsxziptotxtzip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XlsxZipToTxtZipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
